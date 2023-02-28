6c2301887957047aaf392a7cd9fd15ffe438a05a
Joanna <jjasinska.coding@gmail.com>
2023-02-28 10:42:03 +0100

    simplelightbox implemented

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,15 +1,14 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const gallery = document.querySelector(".gallery");
+const galleryDiv = document.querySelector(".gallery");
 
 for (const gi of galleryItems) {
-  gallery.append(document.createElement("div"));
+  galleryDiv.append(document.createElement("div"));
   const div =
     document.querySelectorAll(".gallery div")[
       document.querySelectorAll(".gallery div").length - 1
     ];
-  //div.classList.add("gallery__item");
   div.append(document.createElement("a"));
   const link = div.querySelector("a");
   link.append(document.createElement("img"));
@@ -20,22 +19,12 @@ for (const gi of galleryItems) {
   img.src = gi.preview;
   img.alt = gi.description;
   img.dataset.source = gi.original;
+
   link.onclick = (e) => {
     e.preventDefault();
-    const lightbox = basicLightbox.create(
-      `
-		<img width="1400" height="900" src="${img.dataset.source}">
-	`
-    );
-
-    lightbox.show();
-    document.addEventListener("keydown", closeGallery);
-    function closeGallery(e) {
-      //console.log(e);
-      if (e.key == "Escape") {
-        lightbox.close();
-        document.removeEventListener("keydown", closeGallery);
-      }
-    }
   };
 }
+let gallery = new SimpleLightbox(".gallery a");
+gallery.on("show.simplelightbox", function () {
+  // nothing xD
+});
